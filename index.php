c0b8f4af1dc54edb3c718f28eb41509776ed9578
CristianMor <Cristian.moreno@acuicultec.mx>
2023-02-06 15:03:41 -0700

    Acomodo de la etiqueta php dentro de index.php

--- index.php
+++ index.php
@@ -18,45 +18,43 @@
       <!-- <th>PROMEDIO ACTUAL</th> -->
       <!-- <th>FECHA DE INGRESO</th> -->
     </tr>
-  
+    <?php
 
-  <?php
+      // variables para la conexion //
+      $host = "localhost";
+      $usuario = "root";
+      $contraseña = ".Root123";
+      $nombrebd = "trabajo_extra";
 
-    // variables para la conexion //
-    $host = "localhost";
-    $usuario = "root";
-    $contraseña = ".Root123";
-    $nombrebd = "trabajo_extra";
+      // conexion bd // 
+      $con = mysqli_connect($host,$usuario,$contraseña,$nombrebd);
+      if(!$con){ 
+        // si la conexion de error ejecutara esto //
+        echo "Ocurrio un error en la conexion ala base de datos";
+      } 
+      // echo "la conexion se establecio con exito";
 
-    // conexion bd // 
-    $con = mysqli_connect($host,$usuario,$contraseña,$nombrebd);
-    if(!$con){ 
-      // si la conexion de error ejecutara esto //
-      echo "Ocurrio un error en la conexion ala base de datos";
-    } 
-    // echo "la conexion se establecio con exito";
+      // crear consulta //
+      $consulta = "SELECT * FROM materias_primas";
 
-    // crear consulta //
-    $consulta = "SELECT * FROM materias_primas";
+      $resultado = mysqli_query($con, $consulta); 
+      if(mysqli_num_rows($resultado) > 0){
+      while($row=mysqli_fetch_assoc($resultado)){
+        echo "<tr>";
+        echo '<td>'.$row['id'].'</br>';
+        echo '<td>'.$row['nombre'].'</br>';
+        echo '<td>'.$row['cantidad'].'</br>';
+        echo '<td>'.$row['unidad_de_medidas'].'</br>';
+        // echo '<td>'.$row['PROMEDIO_ACTUAL'].'</br>';
+        // echo '<td>'.$row['FECHA_INGRESO'].'</br>';
+        echo  "</tr>";
+      }
 
-    $resultado = mysqli_query($con, $consulta); 
-    if(mysqli_num_rows($resultado) > 0){
-    while($row=mysqli_fetch_assoc($resultado)){
-      echo "<tr>";
-      echo '<td>'.$row['id'].'</br>';
-      echo '<td>'.$row['nombre'].'</br>';
-      echo '<td>'.$row['cantidad'].'</br>';
-      echo '<td>'.$row['unidad_de_medidas'].'</br>';
-      // echo '<td>'.$row['PROMEDIO_ACTUAL'].'</br>';
-      // echo '<td>'.$row['FECHA_INGRESO'].'</br>';
-      echo  "</tr>";
-    }
+      } else{
+        echo "no existe registro";
+      }
 
-    } else{
-      echo "no existe registro";
-    }
-
-  ?> 
+    ?> 
   </table>
 </body>
 </html>
